dffdd69dcd161901868a6de44eba51c3fba59c44
Christian Casper Hofma <cchofma@live.dk>
2022-08-31 15:16:55 +0200

    First commit

---
+++ src/main/java/com/example/webapimoviedb/WebApiMovieDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.webapimoviedb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApiMovieDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebApiMovieDbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/webapimoviedb/WebApiMovieDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webapimoviedb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApiMovieDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
