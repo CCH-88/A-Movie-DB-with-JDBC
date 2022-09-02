ccd988d4e0546f1e10c3363701fe1fb04b7292aa
PHanse <peterhansendev@gmail.com>
2022-09-02 15:09:51 +0200

    MovieMapper

--- src/main/java/com/example/webapimoviedb/mappers/MovieMapper.java
+++ src/main/java/com/example/webapimoviedb/mappers/MovieMapper.java
@@ -1,4 +1,12 @@
 package com.example.webapimoviedb.mappers;
 
+import com.example.webapimoviedb.models.Movie;
+import com.example.webapimoviedb.models.movie.MovieDTO;
+import org.springframework.web.bind.annotation.Mapping;
+
+@Mapper(componentModel = "spring")
 public interface MovieMapper {
+    MovieDTO movieToMovieDTO(Movie movie);
+    @Mapping(target = "student", ignore = true)
+    Movie movieToMovieDTO(Movie movie);
 }
